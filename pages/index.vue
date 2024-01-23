9e1c13412613e2d5f700add1ab00b256b8175b25
Valenti Gamez <vgr.gamez@gmail.com>
2024-01-23 11:58:08 +0100

    Added bookers village as example.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,82 @@
 <template>
   <div>
+    <!-- Case Study Hero -->
+    <section class="tw-px-4 tw-py-12 md:tw-py-16 sm:tw-px-6 lg:tw-px-8">
+      <div class="tw-max-w-screen-xl tw-mx-auto">
+        <!-- Hero text -->
+        <div class="tw-w-full tw-max-w-3xl tw-mx-auto tw-text-center lg:tw-max-w-5xl">
+          <p
+            class="tw-inline-flex tw-items-center tw-justify-center tw-px-6 tw-py-2 tw-text-sm tw-font-medium tw-tracking-wide tw-text-white tw-rounded-r-full tw-rounded-tl-full tw-bg-gradient-to-r tw-from-dark-600 tw-to-dark-700"
+          >
+            Nos aliamos contigo para crecer juntos
+          </p>
+          <h1
+            class="tw-mt-4 tw-text-4xl tw-font-extrabold tw-text-white md:tw-mt-5 sm:tw-text-5xl md:tw-text-6xl"
+          >
+            Creamos juegos que mueven comunidades
+          </h1>
+        </div>
+      </div>
+
+      <div class="tw-grid tw-justify-center tw-w-full tw-gap-12 tw-mx-auto tw-mt-12 lg:tw-grid-cols-2 lg:tw-gap-8 lg:tw-mt-16 lg:tw-max-w-screen-xl xl:tw-max-w-screen-xl lg:tw-justify-start">
+        <!-- Image collage right -->
+        <div class="tw-flex tw-items-center tw-justify-center tw-max-w-screen-md lg:tw-max-w-full">
+          <div class="tw-grid tw-h-full tw-grid-flow-col tw-grid-rows-2 tw-gap-4">
+            <div class="tw-shadow-xl tw-rounded-3xl">
+              <img src="/img/games/bookersVillage/bookersVillageMonje.jpg" class="tw-object-cover tw-object-center tw-w-full tw-h-full tw-rounded-3xl">
+            </div>
+            <div class="tw-shadow-xl tw-rounded-3xl">
+              <img src="/img/games/bookersVillage/bookersVillageGranjero.jpg" class="tw-object-cover tw-object-center tw-w-full tw-h-full tw-rounded-3xl">
+            </div>
+            <div class="tw-row-span-2 tw-shadow-xl tw-rounded-3xl">
+              <img src="/img/games/bookersVillage/bookersVillageFighter.jpg" class="tw-object-cover tw-object-center tw-w-full tw-h-full tw-rounded-3xl">
+            </div>
+          </div>
+        </div>
+
+        <!-- Image collage left -->
+        <div class="tw-flex tw-items-center tw-justify-center tw-max-w-screen-md lg:tw-max-w-full">
+          <div class="tw-grid tw-h-full tw-grid-flow-col tw-grid-rows-2 tw-gap-4">
+            <div class="tw-row-span-2 tw-shadow-xl tw-rounded-3xl">
+              <img src="/img/games/bookersVillage/bookersVillageEstudiante.jpg" class="tw-object-cover tw-object-center tw-w-full tw-h-full tw-rounded-3xl">
+            </div>
+            <div class="tw-shadow-xl tw-rounded-3xl">
+              <img src="/img/games/bookersVillage/bookersVillageDoctor.jpg" class="tw-object-cover tw-object-center tw-w-full tw-h-full tw-rounded-3xl">
+            </div>
+            <div class="tw-shadow-xl tw-rounded-3xl">
+              <img src="/img/games/bookersVillage/bookersVillageAliado.jpg" class="tw-object-cover tw-object-center tw-w-full tw-h-full tw-rounded-3xl">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Buscamos creadores section -->
+    <section class="tw-max-w-screen-xl tw-px-4 tw-py-12 tw-mx-auto md:tw-py-16 sm:tw-px-6 lg:tw-px-8">
+      <!-- CTA card -->
+      <div class="tw-relative tw-py-16 tw-rounded-3xl tw-bg-dark-700 lg:tw-py-20">
+        <!-- Right background diagonal -->
+        <svg class="tw-absolute tw-inset-y-0 tw-top-0 tw-z-20 tw-w-1/4 tw-h-full tw-right-1/4 tw-text-dark-700" preserveAspectRatio="none" viewBox="0 0 100 100" fill="currentcolor">
+          <polygon points="0,0 100,0 0,100"></polygon>
+        </svg>
+        <div class="tw-absolute tw-inset-y-0 tw-z-10 tw-w-1/2 tw-h-full tw-left-1/2 tw-bg-dark-800 tw-rounded-r-3xl"></div>
+
+        <!-- CTA content -->
+        <div class="tw-relative tw-z-30 tw-flex tw-flex-col tw-items-center tw-justify-center tw-px-4 tw-mx-auto tw-text-center sm:tw-px-16 lg:tw-flex-row lg:tw-text-left">
+          <div class="tw-max-w-lg tw-text-2xl tw-font-bold sm:tw-text-4xl lg:tw-w-1/2">
+            <h5 class="tw-text-4xl tw-font-extrabold tw-tracking-tight tw-text-white sm:tw-text-5xl">
+              #BookersVillage
+            </h5>
+          </div>
+          <div class="tw-flex tw-justify-center tw-max-w-lg tw-mt-10 lg:tw-w-1/2 lg:tw-mt-0 lg:tw-justify-end">
+            <a href="https://twitter.com/search?q=%23BookersVillage&src=typed_query&f=top" target="_blank" class="tw-flex tw-items-center tw-justify-center tw-w-auto tw-px-8 tw-py-4 tw-text-base tw-font-semibold tw-leading-snug tw-transition tw-ease-in-out tw-bg-white tw-rounded-full tw-h-14 tw-duration-250 tw-text-dark-900 hover:tw-text-white focus:tw-outline-none hover:tw-bg-dark-900">
+              Consulta el hashtag
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <!-- Hero message -->
     <section class="tw-px-4 tw-pt-6 tw-pb-12 tw-mx-auto tw-max-w-screen-2xl md:tw-pb-16 sm:tw-px-6 lg:tw-px-8">
       <!-- Hero card -->
