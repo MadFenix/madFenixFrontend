63428bf31dec864371012c08138d0077a448dc33
Valenti Gamez <vgr.gamez@gmail.com>
2024-04-29 10:34:10 +0200

    Added path general to login.

--- store/user.js
+++ store/user.js
@@ -8,7 +8,8 @@ export const actions = {
     this.$axios.setToken(token, 'Bearer')
     //window.document.cookie = 'token=' + token
     this.$cookies.set('token', token, {
-      maxAge: 60 * 60 * 24 * 365 // 1 year
+      maxAge: 60 * 60 * 24 * 365, // 1 year
+      path: '/',
     })
     state.commit('updateToken', token)
   },
@@ -19,7 +20,8 @@ export const actions = {
         if (response.data && response.data.data) {
           //window.document.cookie = 'user=' + JSON.stringify(response.data.data)
           this.$cookies.set('user', JSON.stringify(response.data.data), {
-            maxAge: 60 * 60 * 24 * 365 // 1 year
+            maxAge: 60 * 60 * 24 * 365, // 1 year
+            path: '/',
           })
           state.commit('updateUser', response.data.data)
         } else {
